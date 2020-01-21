3d53ac1cf6da2b6d275f8667d8db67062b675cba
kchang00 <k_chang@fanshaweonline.ca>
2020-01-21 16:53:38 -0500

    adding live data

--- js/main.js
+++ js/main.js
@@ -1,23 +1,78 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-const myVM = (() => {
-    //instantiate a new Vue instance
-    let vue_m = new Vue({
-        data: {
-            message: "Hello from Vue!",
+var vm = new Vue({
+  el: "#app",
 
-            //array of objects
-            collection: [
-                { name: "Kayla", role: "Animator" },
-                { name: "Holly", role: "English Prof" },
-                { name: "Claire", role: "Architect" }
-            ]
-        },
+  data: {
 
-        methods: {
-            logClicked() {
-                console.log('clicked on an element!');
-            }
-        }
-    }).$mount("#app");
-})();
+    // mock up the user - this well eventually come from the database UMS (user management system)
+
+    // this is an object
+    user: {
+        // keys and values - describes user
+        // waiting for user data to be passed in
+    },
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    videotitle:"video title here",
+    videodescription: "vid desc here",
+    videosource:"",
+
+    showDetails: false
+  },
+
+  created: function() {
+    // vue instance is ready - mostly. Add some live data to the VM
+    this.fetchUsers();
+  },
+
+  methods: {
+    logInOut() {
+        // test the login/logout UI -> btn should change colour
+        // eventually we'll use routing and a login component
+        console.log('do login / logout on click');
+        // ? : ; = shorthand if statement - known as a ternary statement
+        // evaluate the expression; if true, use value on the left. If false, use value on right
+        this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    setUserPrefs() {
+        console.log('set user prefs via routing and probably a component');
+    },
+
+    loadMovie({name, description, vidsource}){
+        console.log('show movie details');
+
+        this.videotitle = name;
+        this.videodescription = description;
+        this.videosource = vidsource;
+
+        this.showDetails = true;
+    },
+
+    fetchUsers() {
+      // get user data here and push it back into the VM
+      console.log('fetch user data here');
+
+      const url = './includes/index.php?user=true';
+
+      //promise
+
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+
+        // push our user data into the VM
+        this.user= data[0];
+      })
+      .catch((err) => console.log(err))
+    }
+  }
+});
